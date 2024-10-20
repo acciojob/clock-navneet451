ddf77dcb4547c9f71c818f2abe10127ab825c837
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-20 14:10:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let para=getElementById("timer");
+let timer=new Date();
+setInterval( function(){
+	timer=new Date();
+	para.innerHTML=timer.toLocalString();
+},1000);
